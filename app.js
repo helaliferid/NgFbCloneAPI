d56b656aec22ed1cc3b388f98a72678457948809
Ferid HELALI <helaliferid@gmail.com>
2022-03-01 12:48:32 +0100

    allow Cross Origin scripting

--- app.js
+++ app.js
@@ -3,13 +3,14 @@ const express = require('express')
 const path = require('path');
 const logger = require('morgan');const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
-const movieRouter = require('./routes/movie')
+const movieRouter = require('./routes/movie');
+const cors = require('cors')
 
 
 const app = express();
 
 
-
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
